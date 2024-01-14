80dc62760cecd0788fc2ab76658b90b1c906531c
Mitchell Opitz <mitchell@mitchellopitz.net>
2024-01-14 00:05:20 -0600

    Add pause feature

---
+++ Assets/Misc/Scripts/PauseGame.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+}
